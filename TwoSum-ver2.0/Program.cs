b561cd52f3772ec2b79e8f3c9f93e9fda01aed1e
Евгений <evgeniibelov1401@gmail.com>
2025-04-10 21:55:52 +0300

    сделал задачу без вложенных массивов

--- TwoSum-ver2.0/Program.cs
+++ TwoSum-ver2.0/Program.cs
@@ -1,10 +1,56 @@
 ﻿namespace TwoSum_ver2._0
 {
+    /*
+      Учитывая массив целых чисел nums и целое число target, верните индексы двух чисел, сумма которых равна target.
+      Вы можете предположить, что для каждого ввода будет ровноодно решение, и вы не можете использовать один и тот
+      же элемент дважды.
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Random rnd = new();
+                int[] arr = new int[rnd.Next(2, 20)];
+                int sum = rnd.Next(2, 18);
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = rnd.Next(1, 9);
+                    Console.Write(arr[i] + " ");
+                }
+                Console.WriteLine($"\nЧисло: {sum}");
+
+                var resultArray = TwoSum(arr, sum);
+
+                if (resultArray.Length == 2)
+                {
+                    Console.Write("\nИндексы: ");
+                    for (int i = 0; i < resultArray.Length; i++)
+                    {
+                        Console.Write(resultArray[i] + " ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nНет результата...");
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+        static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var proxy = target - nums[i];
+                int i2 = Array.IndexOf(nums, proxy);
+                if (i2 != -1)
+                {
+                    return new int[] { i, i2 };
+                }
+            }
+            return new int[0];
         }
     }
 }
